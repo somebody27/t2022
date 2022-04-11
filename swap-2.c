6129ab7353646db67b1c0d8dc51f9c8e17df4960
somebody27 <46376373+somebody27@users.noreply.github.com>
2022-04-11 14:43:19 +0800

    通过改变指针指向的内容，实现交换
    gcc swap-2.c -o hello -Wall
    a = 0x7ffed39f1d30,  b = 0x7ffed39f1d34
    a = 0x7ffed39f1d30,  b = 0x7ffed39f1d34
    a = 32,  b = 16

---
+++ swap-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void
+swap ( int *a, int *b ){
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp; 
+}
+int
+main ( int argc, char **argv ){
+    int a,b;
+    a = 16;
+    b = 32;
+    printf("a = %p,  b = %p\n", &a , &b );
+    swap(&a, &b);
+    printf("a = %p,  b = %p\n", &a , &b );
+    printf("a = %d,  b = %d\n", a , b );
+}
